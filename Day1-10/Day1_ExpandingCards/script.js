ed1059c2570c8c6a7cb070beef6b10fc571bfc0d
Kaiyang Yao <yaoky2008@gmail.com>
2023-01-17 20:36:42 -0600

    adjust format

--- Day1-10/Day1_ExpandingCards/script.js
+++ Day1-10/Day1_ExpandingCards/script.js
@@ -1,17 +1,17 @@
-const panels = document.querySelectorAll('.panel')  // Returns a NodeList
+const panels = document.querySelectorAll(".panel"); // Returns a NodeList
 // console.log(panels);
 // console.log(panels[0]);
 // console.log(typeof panels[0]);  // It is an DOM object
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active");
+    });
+});
 
 function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    });
 }
